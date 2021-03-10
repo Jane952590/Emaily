df29968bd08fae0345162b3f94ce7538f36fec97
JaneXiong <862546684@qq.com>
2021-03-10 11:04:04 +0800

    authorized

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+require("./services/passport");
+
 const app = express();
-app.get("/", (req, res) => {
-  res.send({ hi: "buddy" });
-});
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
